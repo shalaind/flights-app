a14de5d5bd103d118a3ae142a441528040cda73e
Shalain DeRamus <shalainderamus@PSADMINs-MacBook-Air-55.local>
2019-03-08 19:49:36 -0500

    added axios call to get flight info

--- client/src/components/FlightList.jsx
+++ client/src/components/FlightList.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import axios from 'axios' 
 
 export default class FlightList extends Component {
+
+    state = {
+        flights: []
+      };
+
+      componentDidMount() {
+        this.getFlights();
+        console.log(this.state.flights)
+      }
+    
+    
+    getFlights = () => {
+        axios.get('/flights').then( res => 
+
+            this.setState({ flights: res.data.flights})
+        
+        ); 
+    }; 
+
+
   render() {
     return (
       <div>
         <h1> list of all flights here </h1>
+
+        {this.state.flights.map((flight, i) => (
+            <div key={i}> 
+               <div>
+               {flight.airline} 
+               </div> 
+               <div>
+                {flight.departs.airport}
+               </div>
+               <div>
+                {flight.arrives.airport}
+               </div>
+            </div>
+        )
+        )}
+
+    
       </div>
     )
   }
